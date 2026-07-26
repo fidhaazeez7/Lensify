26ed2517e863e4374a0ce9f6753f45eb0e42c90a
FIDHA FATHIMA <fidhaazeezv@gmail.com>
2026-07-26 21:11:39 +0530

    Allow new frontend URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://lensify-frontend-fc9o.onrender.com",
+        "https://lensify-ai.onrender.com",   
     ],
     allow_credentials=True,
     allow_methods=["*"],
